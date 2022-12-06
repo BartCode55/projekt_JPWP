a55182762e3681a7904eeea0fcab10120b4263c0
s184354 <b.tupaj@o2.pl>
2022-12-06 19:21:29 +0100

    Zmiany w GameFrame i dodanie buttonow menuPanel

--- GameFrame.java
+++ GameFrame.java
@@ -2,50 +2,50 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class GameFrame extends JFrame implements MouseListener{
+public class GameFrame extends JFrame implements ActionListener{
 	
 	JLabel titleLabel;
 	JLabel menuLabel;
 	JPanel menuPanel;
 	JPanel titlePanel;
 	JPanel gamePanel;
+	JButton[] menuButtons = new JButton[3];
 	ImageIcon bomb = new ImageIcon("bomb.png");
 	ImageIcon bomb2 = new ImageIcon("bomb.png");
 
 	GameFrame(){
 		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(1280,1024);//potem zmienic na 1024 lub tak zostawic zobaczymy
-		this.setLayout(new BorderLayout());
+		this.setSize(1280,1024);
+		this.setLayout(null);
 		this.setResizable(false);
 		this.getContentPane().setBackground(new Color(0x8D91C7));
 		createGamePanel();
 		createMenuPanel();
 		createTitlePanel();
 		
-		this.add(menuPanel, BorderLayout.EAST);
-		this.add(titlePanel, BorderLayout.NORTH);
-		this.add(gamePanel, BorderLayout.CENTER);
-		//this.add(titlePanel);
-		//this.add(gamePanel);
+		this.add(menuPanel); //, BorderLayout.EAST
+		this.add(titlePanel); //, BorderLayout.NORTH
+		this.add(gamePanel); //, BorderLayout.CENTER
 		
 		this.setVisible(true);
-		
-		
 	}
 	
 	public void createGamePanel()
 	{
 		gamePanel = new JPanel();
+		//gamePanel.setLayout(null);
 		gamePanel.setBackground(new Color(0x313DE6));
+		gamePanel.setBounds(0, 100, 1080, 924);
 		//gamePanel.setPreferredSize(new Dimension(1080,200));
 		
 	}
@@ -53,24 +53,61 @@ public class GameFrame extends JFrame implements MouseListener{
 	{
 		menuPanel = new JPanel();
 		menuPanel.setBackground(new Color(0x353C9F));
-		menuPanel.setPreferredSize(new Dimension(200,200));
+		menuPanel.setLayout(null);
+		//menuPanel.setPreferredSize(new Dimension(200,200));
+		menuPanel.setBounds(1080, 100, 200, 924);
+		
+		//dodac labele odpowiedzialne za licznik bomb oraz za czasomierz
 		
 		menuLabel = new JLabel();//ROZWINAC JLABEL
+		
+		for (int i=0; i<3; i++)
+		{
+			menuButtons[i] = new JButton();
+			menuPanel.add(menuButtons[i]);
+			menuButtons[i].setFocusable(false);
+		}
+		
+		menuButtons[0].setBounds(10, 375, 170, 100);
+		menuButtons[0].setText("Reset");
+		menuButtons[0].setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		
+		menuButtons[1].setBounds(10, 500, 170, 100);
+		menuButtons[1].setText("Trudniejszy poziom");
+		menuButtons[1].setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		
+		menuButtons[2].setBounds(10, 625, 170, 100);
+		menuButtons[2].setText("Wyjscie z Gry");
+		menuButtons[2].setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		
+		
+		menuLabel.setBounds(0, 250, 200, 100);
+		//menuLabel.setBackground(Color.GREEN);
+		menuLabel.setText("Menu:");
+		menuLabel.setForeground(new Color(0x27272C));//
+		menuLabel.setVerticalAlignment(JLabel.BOTTOM);
+		menuLabel.setHorizontalAlignment(JLabel.CENTER);
+		menuLabel.setFont(new Font("Times New Roman", Font.BOLD, 35));
+		
+		menuPanel.add(menuLabel);
+		
+		
+		
 	}
 	public void createTitlePanel()
 	{
 		titlePanel = new JPanel();
 		titlePanel.setBackground(new Color(0x8A90DF));
-		titlePanel.setPreferredSize(new Dimension(200,100));
+		titlePanel.setBounds(0,0, 1280, 100);
+		//titlePanel.setPreferredSize(new Dimension(200,100));
 		
 		titleLabel = new JLabel();
 		titleLabel.setText("Bombowa Lamiglowka");
 		titleLabel.setHorizontalTextPosition(JLabel.LEFT);
 		//titleLabel.setVerticalAlignment(JLabel.TOP);
-		titleLabel.setFont(new Font("MV Boli", Font.PLAIN, 60));
+		titleLabel.setFont(new Font("MV Boli", Font.PLAIN, 59));
+		titleLabel.setForeground(new Color(0x404047));
 		titleLabel.setIcon(bomb);
-		
-		
 		
 		titlePanel.add(titleLabel);
 		
@@ -78,32 +115,9 @@ public class GameFrame extends JFrame implements MouseListener{
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {
-		System.out.println("Hello");
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 }
